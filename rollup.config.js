7ea91636fbf40ff6c7e6156fb550bd154762d614
romanyanke <roman@yanke.ru>
2022-09-04 13:05:30 +0300

    use rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
@@ -36,5 +37,6 @@ export default {
       clean: true,
     }),
     commonjs(),
+    terser(),
   ],
 }
